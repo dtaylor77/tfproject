8195777fdeaf7396833bad6eba348bd0b70ecdd6
dtaylor77 <dhavaltailor_07@hotmail.com>
2023-04-04 12:52:31 +0530

    firstvpc

--- dev.tf
+++ dev.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.61.0"
+    }
+  }
+}
+
 resource "aws_vpc" "my_vpc" {
   cidr_block = "172.16.0.0/16"
 
