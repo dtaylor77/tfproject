ed495e3f21aedaba72594436de7dca08cdbaf6f3
dtaylor77 <dhavaltailor_07@hotmail.com>
2023-05-15 19:47:49 -0300

    create s3 bucket

--- dev.tf
+++ dev.tf
@@ -4,12 +4,3 @@ resource "aws_ssm_parameter" "foo" {
   type  = "String"
   value = "bar"
 }
-
-resource "aws_s3_bucket" "example" {
-  bucket = "my-test-tf-bucket-oidc12347"
-
-  tags = {
-    Name        = "My-oidc-bucket"
-    Environment = "Dev"
-  }
-}
