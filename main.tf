6d4055cbf1e2b2240e7feedc37a9da985641c466
dtaylor77 <dhavaltailor_07@hotmail.com>
2023-06-03 11:28:30 -0300

    add vpc

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "aws_ssm_parameter" "foo" {
   value = "bar"
 }
 
+resource "aws_vpc" "test-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "test-vpc"
+  }
+}
