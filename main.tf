0775d09bb8b4eb7c18838ecf26e83247aaac4873
dtaylor77 <dhavaltailor_07@hotmail.com>
2023-06-03 11:50:48 -0300

    create resource

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_ssm_parameter" "foo" {
 }
 
 resource "aws_vpc" "test-vpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+  cidr_block       = "10.5.0.0/16"
+  instance_tenancy = "Default"
 
   tags = {
     Name = "test-vpc"
