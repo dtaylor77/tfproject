f6b0a4de5f8163ebc260063c72b67ae428e0e7bc
dtaylor77 <dhavaltailor_07@hotmail.com>
2023-06-14 10:25:59 -0300

    Deploy new AWS Resource

--- main.tf
+++ main.tf
@@ -1,19 +1,29 @@
 
-resource "aws_ssm_parameter" "foo" {
-  name  = "foobar3"
-  type  = "String"
-  value = "bar"
-}
+# resource "aws_ssm_parameter" "foo" {
+#   name  = "foobar3"
+#   type  = "String"
+#   value = "bar"
+# }
 
-resource "aws_vpc" "main" {
-  cidr_block       = "10.1.0.0/16"
-  instance_tenancy = "default"
+# resource "aws_vpc" "main" {
+#   cidr_block       = "10.1.0.0/16"
+#   instance_tenancy = "default"
 
+#   tags = {
+#     Name = "main-vpc"
+#   }
+# }
+
+# output "filename" {
+#   value = "${path.module}"
+# }
+
+resource "aws_instance" "webserver" {
+  ami = "ami-0d78d8707cd9c1be8"
+  instance_type = "t2.micro"
+  #key_name = file("${path.module}/id_rsa_pub")
+  key_name = "canadakey"
   tags = {
-    Name = "main-vpc"
+    Name = "test-instance"
   }
-}
-
-output "filename" {
-  value = "${path.module}"
 }
