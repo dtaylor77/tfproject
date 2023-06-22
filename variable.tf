b4a0d75da31664555c28996e70ba94b7d49634c4
dtaylor77 <dhavaltailor_07@hotmail.com>
2023-06-21 22:30:13 -0300

    new project

--- variable.tf
+++ variable.tf
@@ -6,10 +6,10 @@ variable "cidr_block_for_SG" {
   
 }
 
-variable "ami_id" {
-  type = string
-}
+# variable "ami_id" {
+#   type = string
+# }
 
-variable "instance_type" {
-  type = string
-}
+# variable "instance_type" {
+#   type = string
+# }
