82560459e3cd836bc42d92a555fe26c6231fddf6
dtaylor77 <129479718+dtaylor77@users.noreply.github.com>
2023-06-21 23:43:16 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@
 resource "aws_instance" "webserver" {
   ami           = "ami-0d04e6652cb408e57"
   instance_type = "t2.micro"
+  availability_zone = "ca-central-1a"
   #key_name = file("${path.module}/id_rsa_pub")
   key_name = "canadakey"
   vpc_security_group_ids = [aws_security_group.allow_ports.id]
@@ -34,6 +35,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "dev_subnet" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.7.1.0/24"
+  availability_zone = "ca-central-1a"
 
   tags = {
     Name = "dev_subnet"
