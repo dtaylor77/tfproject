924d4b5bb0ef9249bdd123cf46395362d5b2e51d
dtaylor77 <129479718+dtaylor77@users.noreply.github.com>
2023-08-01 12:45:25 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,10 +6,10 @@
 #}
 #}
 
- provider "aws" {
-   version = "5.1.0"
-   region  = "ca-central-1"
- }
+# provider "aws" {
+ #  version = "5.1.0"
+ #  region  = "ca-central-1"
+ #}
 
 # terraform {
 #   backend "s3" {
