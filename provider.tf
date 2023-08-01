58afa3bb8cf5d0408789e25aaff3042c7efdfdcd
dtaylor77 <129479718+dtaylor77@users.noreply.github.com>
2023-08-01 12:35:23 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -21,11 +21,11 @@ provider "aws" {
   region = "ca-central-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "github-oidc-terraform-aws-state-file"
-    region = "ca-central-1"
-    key    = "github-oidc-terraform.tfstate"
+#terraform {
+ # backend "s3" {
+ #   bucket = "github-oidc-terraform-aws-state-file"
+  #  region = "ca-central-1"
+ #  key    = "github-oidc-terraform.tfstate"
 
-  }
-}
+ # }
+#}
